cb428eba897362b321da97a04528b3f231d83d64
sar0868 <safarov-ar@yandex.ru>
2025-05-23 12:05:49 +0300

    create branch, add script labs_2_9.sh

---
+++ labs_2_9.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo "Привет"
+echo "Мое имя Алексей"
+echo "Сейчас я нахожусь в $PWD"
+echo "Мой домашний каталог $HOME"
+echo "Моя ОС"
+cat /etc/os-release
+echo "Мое время работы"
+uptime
